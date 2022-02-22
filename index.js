6da8a2f18fc2cff87a1db88d4bfad4b46c70ffe1
AFEDev <algirduj@gmail.com>
2022-02-22 17:55:59 +0200

    done

---
+++ index.js
@@ -0,0 +1,3 @@
+const selector = document.querySelector("#action").value;
+
+console.log(selector);
---
+++ js/calc.js
@@ -0,0 +1,22 @@
+const formDOM = document.querySelector(".form");
+const actionDOM = document.querySelector("#action").value;
+const firstNumber = document.querySelector("#first_number");
+const secondNumber = document.querySelector("#second_number");
+const submitDOM = document.querySelector("button");
+const resultDOM = document.querySelector(".result");
+const matematika = {
+    "+": (a, b) => { return a + b; },
+    "-": (a, b) => { return a - b; },
+    "*": (a, b) => { return a * b; },
+    "/": (a, b) => { return a / b; },
+};
+submitDOM.addEventListener('click', (e) => {
+    e.preventDefault();
+    parseInt(firstNumber.value);
+    parseInt(secondNumber.value);
+    const veiksmas = actionDOM;
+    const res = matematika[veiksmas](parseInt(firstNumber.value), parseInt(secondNumber.value));
+    console.log(res);
+    return resultDOM.innerHTML = `${firstNumber.value} ${veiksmas} ${secondNumber.value} = ${res}`;
+});
+//# sourceMappingURL=calc.js.map
